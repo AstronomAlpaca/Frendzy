8d52d96a702027b6b3e0a92dba9ba85fa769dbad
AstronomAlpaca <stevenrockett@gmail.com>
2022-06-30 20:31:21 +0100

    basic express iteration

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
