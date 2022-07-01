d60e836cd4ba537902c9812bd969cf51f18b9aee
AstronomAlpaca <stevenrockett@gmail.com>
2022-07-01 16:50:07 +0100

    Initialise MongoDB Atlas connection. Prepare future designations (see comments)

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+// middleware - todo
+
+// routes - todo
+
+// DB connection
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    // server listening
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to DB. Listening on port ${process.env.PORT}...`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
