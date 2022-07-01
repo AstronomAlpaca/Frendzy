ecfc466caab7e342879270d8b3f7317be72d0c7c
AstronomAlpaca <stevenrockett@gmail.com>
2022-07-01 19:43:47 +0100

    introducing controllers. utilise with routes

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,9 @@
+const User = require("../models/user.model");
+
+const confirmWorking = () => {
+  alert("Working!");
+};
+
+module.exports = {
+  confirmWorking,
+};
--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const usersRouter = express.Router();
+const { confirmWorking } = require("../controllers/users.controller");
 
-usersRouter.get("/", () => {});
+usersRouter.get("/", confirmWorking);
 
 module.exports = usersRouter;
