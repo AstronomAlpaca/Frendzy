e5f7a1ee23602db67b98f6a4251307ff9cb01eb9
AstronomAlpaca <stevenrockett@gmail.com>
2022-07-02 21:52:22 +0100

    minor adjustment to logger. commented more test examples in supertest.test

--- server/tests/supertest.test.js
+++ server/tests/supertest.test.js
@@ -8,6 +8,18 @@ test("this route works", async () => {
   await api.get("/api/users/").expect(200);
 });
 
+// test("there are two notes", async () => {
+//   const response = await api.get("/api/notes");
+
+//   expect(response.body).toHaveLength(2);
+// });
+
+// test("the first note is about HTTP methods", async () => {
+//   const response = await api.get("/api/notes");
+
+//   expect(response.body[0].content).toBe("HTML is easy");
+// });
+
 afterAll(() => {
   mongoose.connection.close();
 });
