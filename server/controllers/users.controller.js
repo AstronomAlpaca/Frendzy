e38d04eaced01a00c7222f42204f97519184ccc2
AstronomAlpaca <stevenrockett@gmail.com>
2022-07-03 17:51:33 +0100

    Additional method to return all users on basic user route. See User model for a guide on what should be displayed in console (password being hidden, etc)

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,9 +1,37 @@
+const bcrypt = require("bcrypt");
+const { response } = require("express");
 const User = require("../models/user.model");
 
-const confirmWorking = async (req, res) => {
-  await res.send("working!");
+const createUser = async (req, res) => {
+  const { first_name, surname, email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({
+      error: "email must be unique",
+    });
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    first_name: first_name,
+    surname: surname,
+    email: email,
+    password: passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  res.status(201).json(savedUser);
+};
+
+const returnAllUsers = async (req, res) => {
+  const users = await User.find({});
+  response.json(users);
 };
 
 module.exports = {
-  confirmWorking,
+  createUser,
+  returnAllUsers,
 };
--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const usersRouter = express.Router();
-const { confirmWorking } = require("../controllers/users.controller");
+const {
+  createUser,
+  returnAllUsers,
+} = require("../controllers/users.controller");
 
-usersRouter.get("/", confirmWorking);
+usersRouter.get("/", returnAllUsers);
+usersRouter.post("/register", createUser);
 
 module.exports = usersRouter;
