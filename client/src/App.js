db94e9334558db67da1a02005cfbdb9a82c93900
AstronomAlpaca <stevenrockett@gmail.com>
2022-07-03 20:40:37 +0100

    Basic form template (frontend)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,38 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+import React, { useState } from "react";
+
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log("Logging in with", email, password);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          Email
+          <input
+            type="email"
+            value={email}
+            name="Email"
+            onChange={({ target }) => setEmail(target.value)}
+          />
+        </div>
+        <div>
+          Password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default App;
