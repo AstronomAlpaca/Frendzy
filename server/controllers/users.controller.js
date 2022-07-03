a18d2a9d7ed5148f249acf40bf2a131c631fc735
AstronomAlpaca <stevenrockett@gmail.com>
2022-07-03 20:00:44 +0100

    User login functionality with JWT

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 
@@ -25,6 +26,34 @@ const createUser = async (req, res) => {
   res.status(201).json(savedUser);
 };
 
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return res.status(401).json({
+      error: "failed login attempt. check email and password",
+    });
+  }
+
+  const userForToken = {
+    email: user.email,
+    id: user._id,
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  res.status(200).send({
+    token,
+    email: user.email,
+    first_name: user.first_name,
+    surname: user.surname,
+  });
+};
+
 const returnAllUsers = async (req, res) => {
   const users = await User.find({});
   res.json(users);
@@ -33,4 +62,5 @@ const returnAllUsers = async (req, res) => {
 module.exports = {
   createUser,
   returnAllUsers,
+  userLogin,
 };
--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -3,9 +3,11 @@ const usersRouter = express.Router();
 const {
   createUser,
   returnAllUsers,
+  userLogin,
 } = require("../controllers/users.controller");
 
 usersRouter.get("/", returnAllUsers);
+usersRouter.post("/login", userLogin);
 usersRouter.post("/register", createUser);
 
 module.exports = usersRouter;
