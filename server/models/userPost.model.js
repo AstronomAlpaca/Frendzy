2923a3328c6e9418d40722452cc492e2af7891d7
AstronomAlpaca <stevenrockett@gmail.com>
2022-07-03 18:07:53 +0100

    Set JSON return data on UserPost Model

--- server/models/userPost.model.js
+++ server/models/userPost.model.js
@@ -11,4 +11,12 @@ const UserPostSchema = new Schema({
   },
 });
 
+UserPostSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 module.exports = mongoose.model("UserPost", UserPostSchema);
