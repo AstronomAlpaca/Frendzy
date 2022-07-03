745a037f43acbc3152d24bb696b03a16c9e13be8
AstronomAlpaca <stevenrockett@gmail.com>
2022-07-03 16:50:20 +0100

    adjust what is returned in JSON format from model data

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -18,4 +18,14 @@ const UserSchema = new Schema(
   { timestamps: true }
 );
 
+UserSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
+  },
+});
+
 module.exports = mongoose.model("User", UserSchema);
